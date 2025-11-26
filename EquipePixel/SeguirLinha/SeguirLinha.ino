9ee61806069603db3c3e202baebb4bb81ba757e6
Caio Samuel <samuel451200@gmail.com>
2025-11-26 09:23:48 -0300

    SeguirLinha

---
+++ EquipePixel/SeguirLinha/SeguirLinha.ino
@@ -0,0 +1,56 @@
+#include <robo_hardware2.h> 
+#include <Servo.h>
+
+//-----PINOS PARA LIGAR MOTORES-----//
+//Motor1:
+//pino1 PWM_RODA_DIREITA 	6
+//pino2 SENTIDO_RODA_DIREITA 	7
+//
+//Motor2:
+//pino1 PWM_RODA_ESQUERDA 	5
+//pino2 SENTIDO_RODA_ESQUERDA	4
+//-----PINOS PARA LIGAR MOTORES-----//
+
+//-----PINOS PARA SENSORES REFLETANCIA-----//
+//SENSOR_LINHA_MAIS_ESQUERDO	    A3
+//SENSOR_LINHA_ESQUERDO			    A2
+//SENSOR_LINHA_DIREITO			    A1	
+//SENSOR_LINHA_MAIS_DIREITO			A0	
+//-----PINOS PARA SENSORES REFLETANCIA-----//
+
+#define DIVISOR_BRANCO_PRETO 75
+
+float valorSensorDir;
+float valorSensorEsq;
+
+void setup(){
+	robo.configurar();
+}
+
+void loop(){
+  valorSensorMaisEsq = robo.lerSensorLinhaMaisEsq(); //le um valor do sensor. O sensor retornar um valor de 0 100
+	valorSensorEsq = robo.lerSensorLinhaEsq(); //Le o valor do sensor esquerdo e coloca dentro da variavel valor_sensor_esq
+	valorSensorDir = robo.lerSensorLinhaDir(); //Le o valor do sensor direito e coloca dentro da variavel valor_sensor_dir
+  valorSensorMaisDir = robo.lerSensorLinhaMaisDir(); //le um valor do sensor. O sensor retornar um valor de 0 100
+
+	if (valorSensorMaisEsq < DIVISOR_BRANCO_PRETO && valorSensorEsq < DIVISOR_BRANCO_PRETO && valorSensorDir < DIVISOR_BRANCO_PRETO && valorSensorMaisDir < DIVISOR_BRANCO_PRETO) {
+    robo.acionarMotores(0, 0);
+  } else if (valorSensorMaisEsq > DIVISOR_BRANCO_PRETO && valorSensorEsq > DIVISOR_BRANCO_PRETO && valorSensorDir > DIVISOR_BRANCO_PRETO && valorSensorMaisDir > DIVISOR_BRANCO_PRETO) {
+    robo.acionarMotores(100, 100);
+  } else if (valorSensorMaisEsq > DIVISOR_BRANCO_PRETO && valorSensorEsq < DIVISOR_BRANCO_PRETO && valorSensorDir < DIVISOR_BRANCO_PRETO && valorSensorMaisDir > DIVISOR_BRANCO_PRETO) {
+    robo.acionarMotores(100, 100);
+  } else if (valorSensorMaisEsq < DIVISOR_BRANCO_PRETO && valorSensorEsq < DIVISOR_BRANCO_PRETO && valorSensorDir < DIVISOR_BRANCO_PRETO && valorSensorMaisDir > DIVISOR_BRANCO_PRETO) {
+    robo.acionarMotores(-100, 100);
+  } else if (valorSensorMaisEsq < DIVISOR_BRANCO_PRETO && valorSensorEsq < DIVISOR_BRANCO_PRETO && valorSensorDir > DIVISOR_BRANCO_PRETO && valorSensorMaisDir > DIVISOR_BRANCO_PRETO) {
+    robo.acionarMotores(-100, 100);
+  } else if (valorSensorMaisEsq > DIVISOR_BRANCO_PRETO && valorSensorEsq < DIVISOR_BRANCO_PRETO && valorSensorDir < DIVISOR_BRANCO_PRETO && valorSensorMaisDir < DIVISOR_BRANCO_PRETO) {
+    robo.acionarMotores(100, -100);
+  } else if (valorSensorMaisEsq > DIVISOR_BRANCO_PRETO && valorSensorEsq > DIVISOR_BRANCO_PRETO && valorSensorDir < DIVISOR_BRANCO_PRETO && valorSensorMaisDir < DIVISOR_BRANCO_PRETO) {
+    robo.acionarMotores(100, -100);
+  } else if (valorSensorMaisEsq > DIVISOR_BRANCO_PRETO && valorSensorEsq > DIVISOR_BRANCO_PRETO && valorSensorDir > DIVISOR_BRANCO_PRETO && valorSensorMaisDir < DIVISOR_BRANCO_PRETO) {
+    robo.acionarMotores(100, -100);
+  } else if (valorSensorMaisEsq < DIVISOR_BRANCO_PRETO && valorSensorEsq > DIVISOR_BRANCO_PRETO && valorSensorDir > DIVISOR_BRANCO_PRETO && valorSensorMaisDir > DIVISOR_BRANCO_PRETO) {
+    robo.acionarMotores(-100, 100);
+  }
+}
+
